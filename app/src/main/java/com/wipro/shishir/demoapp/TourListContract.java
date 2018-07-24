db9c294b8eae152d61937199f8869518d47b2972
shissharma <shissharma@blackberry.com>
2018-07-24 15:27:30 +0530

    optimized code and removed unused imports and identifiers from contract

--- app/src/main/java/com/wipro/shishir/demoapp/TourListContract.java
+++ app/src/main/java/com/wipro/shishir/demoapp/TourListContract.java
@@ -1,12 +1,10 @@
 package com.wipro.shishir.demoapp;
 
-import android.view.MenuItem;
-
 import com.wipro.shishir.demoapp.api.RequestManager;
 import com.wipro.shishir.demoapp.model.MainData;
 
 public interface TourListContract {
-    public interface MainView {
+     interface MainView {
         void showProgress();
         void hideProgress();
         void setAdapter(MainData mainData);
@@ -14,7 +12,7 @@ public interface TourListContract {
         void setTitle(String title);
     }
 
-    public interface MainPresenter extends RequestManager.ResponseListener<MainData> {
+     interface MainPresenter extends RequestManager.ResponseListener<MainData> {
         void startProcess();
         void initiate();
         void onRefresh();
